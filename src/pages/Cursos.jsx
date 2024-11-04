2d22a315e18dc848a841811d6a450ea534ea9ad0
Gabriel Henrique Souza do Nascimento <113404490+Bielhsn@users.noreply.github.com>
2024-11-04 16:15:23 -0300

    Iniciando tela de cursos

--- src/pages/Cursos.jsx
+++ src/pages/Cursos.jsx
@@ -3,51 +3,55 @@ import React from 'react';
 import Navbar from '../components/Navbar'; // Importando a Navbar
 import Footer from '../components/Footer'; // Importando o Footer
 import './css/Cursos.css'; // Importando o CSS da página de Cursos
+import curso_banner from '../assests/images/curso_banner.gif'
+import curso_hemacias from '../assests/images/hemacias.png'
+import curso_primeira_temp from '../assests/images/foto_curso_precisamos.png'
 
 const Cursos = () => {
     return (
         <div className="cursos">
             <Navbar />
-            <div className="bloco-1">
-                <h1>Educação sem distância.</h1>
-                <p>
-                    A imuno&hemato tem como principal objetivo tornar acessível o conhecimento e a educação em imuno-hematologia e hemoterapia por todos que querem e precisam aprender. Se liga nos cursos que temos.
-                </p>
-            </div>
-            <div className="bloco-2">
-                <div className="curso">
-                    <h2>Desvendando o painel de hemácias.</h2>
-                    <p>O curso mais direto e objetivo pra quem tem dificuldade de descobrir qual anticorpo está no painel, que parece mais uma batalha naval.</p>
-                    <button onClick={() => window.open('https://pay.hotmart.com/B48662820F?checkoutMode=10', '_blank')}>Saiba mais</button>
-                    <button onClick={() => window.open('https://pay.hotmart.com/B48662820F?checkoutMode=10', '_blank')}>Inscreva-se</button>
+            {/* Seção do banner */}
+            <section class="cursos-section_banner">
+                <div class="cursos-texto">
+                    <h2>Educação sem distância.</h2>
+                    <p>A imuno&hemato tem como principal objetivo tornar acessível o conhecimento e a educação em imuno-hematologia e hemoterapia por todos que querem e precisam aprender.</p>
+                    <button class="btn-cursos">VER CURSOS ↓</button>
                 </div>
-                <div className="curso">
-                    <h2>Por onde começar?</h2>
-                    <p>Esse curso vai te levar para os primórdios da imunologia e hematologia e te mostrar quanto é importante você saber disso pra estar na bancada.</p>
-                    <button disabled>Saiba mais</button>
-                    <button disabled>Inscrições em breve</button>
+                <div class="cursos-imagem_banner">
+                    <img src={curso_banner} ></img>
                 </div>
-                <div className="curso">
-                    <h2>Dominando a hemoterapia.</h2>
-                    <p>Aqui você vai saber da essência da imuno-hemato. Vamos aprender juntos os principais conceitos de grupos sanguíneos, significado clínico dos anticorpos, técnicas dos testes pré-transfusionais e muito mais.</p>
-                    <button disabled>Inscrições em breve</button>
-                </div>
-                <div className="curso">
-                    <h2>Minicurso transfusão em RN.</h2>
-                    <p>Nesse curso, você vai complementar a sua formação em imuno-hematologia e aprender de forma objetiva e com base científica a resolver problemas avançados que nos deparamos na bancada, aqueles além da rotina mesmo.</p>
-                    <button disabled>Inscrições em breve</button>
-                </div>
-                <div className="curso">
-                    <h2>Atualização em sistema Rh.</h2>
-                    <p>Aqui você vai conhecer bem mais sobre esse sistema que tem tanta particularidade que só mesmo quem estuda ele exclusivamente pode te contar! De forma leve e objetiva, você vai desmistificar a complexidade do Rh.</p>
-                    <button disabled>Inscrições em breve</button>
-                </div>
-                <div className="curso">
-                    <h2>Combos.</h2>
-                    <p>Bom, como o próprio nome diz a gente vai te colocar por dentro de todas as técnicas disponíveis de genotipagem e te explicar como isso funciona. Do jeito que você gosta: de forma objetiva!</p>
-                    <button disabled>Inscrições em breve</button>
-                </div>
-            </div>
+                </section>
+            {/* Seção dos cursos */}
+                {/* Seção desvendando hemácias */}
+                <section className="curso-destaque-hemacias">
+                    <div className="curso-imagem-hemacias">
+                        <img src={curso_hemacias} alt="Desvendando o painel de hemácias" />
+                    </div>
+                    <div className="curso-descricao-hemacias">
+                        <h2>Desvendando o painel de hemácias.</h2>
+                        <p>O curso mais direto e objetivo pra quem tem dificuldade de descobrir qual anticorpo está no painel, que parece mais uma batalha naval.</p>
+                        <div className="curso-botoes-hemacias">
+                        <button className="botao-inscrever-hemacias">INSCREVER-SE AGORA</button>
+                        <button className="botao-saiba-mais-hemacias">SAIBA MAIS</button>
+                        </div>
+                    </div>
+                </section>
+
+                {/* Seção primeira temporada */}
+                <section className="curso-destaque-primeira-temp">
+                    <div className="curso-imagem-primeira-temp">
+                        <img src={curso_primeira_temp} alt="Desvendando o painel de hemácias" />
+                    </div>
+                    <div className="curso-descricao-primeira-temp">
+                        <h2>Desvendando o painel de hemácias.</h2>
+                        <p>O curso mais direto e objetivo pra quem tem dificuldade de descobrir qual anticorpo está no painel, que parece mais uma batalha naval.</p>
+                        <div className="curso-botoes-primeira-temp">
+                        <button className="botao-inscrever-primeira-temp">INSCREVER-SE AGORA</button>
+                        <button className="botao-saiba-mais-primeira-temp">SAIBA MAIS</button>
+                        </div>
+                    </div>
+                </section>
             <Footer />
         </div>
     );
