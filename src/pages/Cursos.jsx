dae683dc3e1ad9805d5b337e42ca3d3b5c10b9bb
Gabriel Henrique Souza do Nascimento <113404490+Bielhsn@users.noreply.github.com>
2024-11-29 15:45:51 -0300

    Trocando fotos do cursos

--- src/pages/Cursos.jsx
+++ src/pages/Cursos.jsx
@@ -8,7 +8,7 @@ import curso_hemacias from "../assests/images/curso_hemacias.png";
 import curso_primeira_temp from "../assests/images/foto_curso_precisamos.png";
 import curso_segunda_temp from "../assests/images/foto_curso_temp2.png";
 import curso_terceira_temp from "../assests/images/rn.png";
-import curso_rh from "../assests/images/rh-curso.png";
+import curso_rh from "../assests/images/rh.png";
 import curso_genotipagem from "../assests/images/hemoterapia_curso.jpg";
 
 const Cursos = () => {
@@ -37,14 +37,6 @@ const Cursos = () => {
           <div className="card-details-hemacias" id= "card-480">
             <img src={curso_hemacias} className="img" alt="Curso Hemácias" id= "img-480" />
             <div className="text-container-hemacias">
-              <p className="text-title-hemacias">
-                Desvendando o painel de hemácias
-              </p>
-              <p className="text-body-hemacias">
-                O curso mais direto e objetivo pra quem tem dificuldade de
-                descobrir qual anticorpo está no painel, que parece mais uma
-                batalha naval.
-              </p>
             </div>
           </div>
           <a href="https://hotmart.com/pt-br/marketplace/produtos/desvendando-o-painel-de-hemacias/B48662820F">
@@ -61,15 +53,6 @@ const Cursos = () => {
               alt="Primeira temporada"
             />
             <div className="text-container-primeira">
-              <p className="text-title-primeira" id="text-title-primeira-480">
-                {" "}
-                Imuno e hemato: Por onde começar?
-              </p>
-              <p className="text-body-primeira">
-                Esse curso vai te levar para os primórdios da imunologia e
-                hematologia e te mostrar quanto é importante você saber disso
-                pra estar na bancada.
-              </p>
             </div>
           </div>
           <a href="https://hotmart.com/pt-br/marketplace/produtos/serie-imuno-e-hemato-1atemporada/S80660889W">
@@ -86,15 +69,6 @@ const Cursos = () => {
               alt="Curso Hemácias"
             />
             <div className="text-container-segunda">
-              <p className="text-title-segunda">
-                Imuno e hemato: imuno-hematologia em sua essência.{" "}
-              </p>
-              <p className="text-body-segunda">
-                Aqui você vai saber da essência da imuno-hemato. Vamos aprender
-                juntos os principais conceitos de grupos sanguíneos, significado
-                clínico dos anticorpos, técnicas dos testes pré-transfusionais e
-                muito mais.
-              </p>
             </div>
           </div>
           <a href="https://hotmart.com/pt-br/marketplace/produtos/serie-imuno-e-hemato-2a-temporada/G85816407E">
@@ -113,15 +87,6 @@ const Cursos = () => {
               alt="Curso Hemácias"
             />
             <div className="text-container-hemacias">
-              <p className="text-title-hemacias">
-                Transfusão em recém-nascidos e pediatria.
-              </p>
-              <p className="text-body-hemacias">
-                Ideal para profissionais que desejam aprofundar seus
-                conhecimentos sobre transfusão de sangue em neonatos e
-                pediatria, abordando tópicos essenciais e específicos para esse
-                público.
-              </p>
             </div>
           </div>
           <a href="https://hotmart.com/pt-br/marketplace/produtos/transfusao-em-recem-nascidos-e-pediatria/G96358968W">
@@ -134,13 +99,6 @@ const Cursos = () => {
           <div className="card-details-primeira">
             <img src={curso_rh} className="img-rh" alt="Primeira temporada" />
             <div className="text-container-primeira">
-              <p className="text-title-primeira"> Atualização em sistema Rh.</p>
-              <p className="text-body-primeira">
-                Aqui você vai conhecer bem mais sobre esse sistema que tem tanta
-                particularidade que só mesmo quem estuda ele exclusivamente pode
-                te contar! De forma leve e objetiva, você vai desmistificar a
-                complexidade do Rh.
-              </p>
             </div>
           </div>
           <a href="https://pay.hotmart.com/B48662820F?checkoutMode=10">
@@ -153,16 +111,6 @@ const Cursos = () => {
           <div className="card-details-segunda">
             <img src={curso_genotipagem} className="img-dominando" alt="Curso Hemácias" />
             <div className="text-container-segunda">
-              <p className="text-title-segunda">
-                Dominando a Hemoterapia: Fundamentos e práticas na enfermagem.{" "}
-              </p>
-              <p className="text-body-segunda">
-                O curso aborda hemoterapia aplicada aos cuidados assistenciais,
-                desde a coleta até a transfusão, destacando etapas técnicas e
-                essenciais do processo transfusional. É indicado para
-                profissionais técnicos que desejam entender a assistência e
-                vice-versa, unindo enfermagem e técnicos de bancada.
-              </p>
             </div>
           </div>
           <a href="https://hotmart.com/pt-br/marketplace/produtos/dominando-a-hemoterapia/Y92002606B">
